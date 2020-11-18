b5751ef0187351d68c8d5e9e7c8cff1e5c0ce05e
Nathan Lima <nathan.o.aguiar@gmail.com>
2020-11-18 09:09:41 -0300

    Criaçao do Projeto

---
+++ src/main/java/com/example/projetospringpoo/ProjetospringpooApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetospringpoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringpooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringpooApplication.class, args);
+	}
+
+}
