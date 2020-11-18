7f74c5f43caa4a12b36c9f4faacf4a6ba905e76b
Nathan Lima <nathan.o.aguiar@gmail.com>
2020-11-18 09:50:14 -0300

    Criando BaseEnity e NamedEnity

---
+++ src/main/java/com/example/projetospringpoo/model/NamedEnity.java
@@ -0,0 +1,20 @@
+package com.example.projetospringpoo.model;
+
+import java.io.Serializable;
+
+public class NamedEnity extends BaseEnity implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
--- src/main/java/com/example/projetospringpoo/resources/CategoryResource.java
+++ src/main/java/com/example/projetospringpoo/resources/CategoryResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryResource {
 
     @GetMapping
-    public String test(){
+    public String test(){               
 
         return "REST Funcionando";
     }
