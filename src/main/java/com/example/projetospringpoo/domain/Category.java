adb6f15013add5d78053fdba21148ee39bcd8da4
Nathan Lima <nathan.o.aguiar@gmail.com>
2020-11-18 11:50:55 -0300

    Testanto primeira classe de dominio - Categoria

---
+++ src/main/java/com/example/projetospringpoo/domain/Category.java
@@ -0,0 +1,14 @@
+package com.example.projetospringpoo.domain;
+
+import java.io.Serializable;
+
+import com.example.projetospringpoo.model.NamedEnity;
+
+public class Category extends NamedEnity implements Serializable{
+
+
+    private static final long serialVersionUID = 1L;
+    
+    
+
+}
